cf90813f9918e072797818e20ec624b1f368fa1c
RomanSeveryn <romaseverin50@gmail.com>
2021-01-01 23:32:27 +0200

    Add example array

--- index.js
+++ index.js
@@ -52,3 +52,50 @@ function getArrayRandom() {
   }
   return arr;
 }
+
+
+
+// Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = array1.concat(array2);
+
+
+
+
+
+
+// Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+const arrayy5 = [1, 2, 3];
+console.log(arrayy5.reverse());
+
+
+
+// Дан массив [1, 2, 3]. Добавьте ему в конец элементы 4, 5, 6.
+
+const array6 = [1, 2, 3];
+array6.push(4, 5, 6);
+
+
+
+
+// Дан массив [1, 2, 3]. Добавьте ему в начало элементы 4, 5, 6.
+
+const array7 = [1, 2, 3];
+array7.unshift(4, 5, 6);
+
+// Дан массив ['js', 'css', 'jq']. Выведите на экран первый элемент и удалите его.
+
+const array8 = ['js', 'css', 'jq'];
+console.log(array8[0]);
+array8.shift();
+
+
+// Дан массив ['js', 'css', 'jq']. Выведите на экран последний элемент.
+const array9 = ['js', 'css', 'jq'];
+console.log(array9[array9.length - 1]);
+
+// Дан массив [1, 2, 3, 4, 5]. С помощью метода slice запишите в новый элементы [1, 2, 3].
+
+const array10 = [1, 2, 3, 4, 5];
+const array11 = array10.slice(0,3);
